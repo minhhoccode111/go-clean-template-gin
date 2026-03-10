40a2dd50f3badb9f6e1ddfabb2433998e4fb5637
minhhoccode111 <minhhoccode111@gmail.com>
2026-03-10 20:00:03 +0700

    chore: minor changes

--- internal/controller/restapi/middleware/recovery.go
+++ internal/controller/restapi/middleware/recovery.go
@@ -5,6 +5,7 @@ import (
 	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
+	"github.com/minhhoccode111/go-clean-template-gin/internal/controller/restapi/v1/response"
 	"github.com/minhhoccode111/go-clean-template-gin/pkg/logger"
 )
 
@@ -20,7 +21,9 @@ func Recovery(l logger.Interface) gin.HandlerFunc {
 					debug.Stack(),
 				)
 
-				c.AbortWithStatus(http.StatusInternalServerError)
+				c.AbortWithStatusJSON(http.StatusInternalServerError, response.Error{
+					Error: "Internal Server Error",
+				})
 			}
 		}()
 
